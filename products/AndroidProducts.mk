a56003f5095f38277415eb8620ceab27ce152caf
snnbyyds <snnbyyds@gmail.com>
2023-12-30 16:30:32 +0800

    products: Introduce liuqin
    Change-Id: I1445767216165697a4d854c2824e10c93376b10c
    Signed-off-by: mujianwu <lrh15563910083@163.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -27,6 +27,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/fuxi/aospa_fuxi.mk \
     $(LOCAL_DIR)/juice/aospa_juice.mk \
     $(LOCAL_DIR)/lisa/aospa_lisa.mk \
+    $(LOCAL_DIR)/liuqin/aospa_liuqin.mk \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
     $(LOCAL_DIR)/lunaa/aospa_lunaa.mk \
     $(LOCAL_DIR)/lynx/aospa_lynx.mk \
@@ -69,6 +70,8 @@ COMMON_LUNCH_CHOICES += \
     aospa_dubai-userdebug \
     aospa_juice-userdebug \
     aospa_lisa-userdebug \
+    aospa_liuqin-user \
+    aospa_liuqin-userdebug \
     aospa_lmi-userdebug \
     aospa_lunaa-userdebug \
     aospa_lynx-userdebug \
